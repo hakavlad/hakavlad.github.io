8095215bef5a2593a3fb1e25c72be6d5ef7cb9ef
hakavlad <hakavlad@gmail.com>
2017-06-23 23:17:45 +0900

    10 commit

---
+++ js/verbs12.js
@@ -0,0 +1,65 @@
+
+var v12en = [
+"love", 
+"live", 
+"work", 
+"open", 
+"close", 
+"see", 
+"come", 
+"go", 
+"know", 
+"think", 
+"start", 
+"finish"
+];
+
+var v12en2 = [
+"loved", 
+"lived", 
+"worked", 
+"opened", 
+"closed", 
+"saw", 
+"came", 
+"went", 
+"knew", 
+"thought", 
+"started", 
+"finished"
+];
+
+var v12ru = [
+"любить", 
+"жить", 
+"работать", 
+"открыть", 
+"закрыть", 
+"видеть", 
+"приходить", 
+"идти", 
+"знать", 
+"думать", 
+"начать", 
+"закончить"
+];
+
+
+
+var verbs12 = [
+"любить</td></tr><tr><td class=ab>Спойлер:</td><td class=spo>love [lʌv] - loved [lʌvd]", 
+"жить</td></tr><tr><td class=ab>Спойлер:</td><td class=spo>live [liv] - lived [livd]", 
+"работать</td></tr><tr><td class=ab>Спойлер:</td><td class=spo>work [wз:k] - worked [wз:kt]", 
+"открыть</td></tr><tr><td class=ab>Спойлер:</td><td class=spo>open ['əup(ə)n] - opened ['əup(ə)nd]", 
+"закрывать</td></tr><tr><td class=ab>Спойлер:</td><td class=spo>close [kləuz] - closed [kləuzt]", 
+"видеть</td></tr><tr><td class=ab>Спойлер:</td><td class=spo>see [si:] - saw [sɔ:]", 
+"приходить</td></tr><tr><td class=ab>Спойлер:</td><td class=spo>come [kʌm] - came [keim]", 
+"идти</td></tr><tr><td class=ab>Спойлер:</td><td class=spo>go [gou] - went [went]", 
+"знать</td></tr><tr><td class=ab>Спойлер:</td><td class=spo>know [nəu] - knew [nju:]", 
+"думать</td></tr><tr><td class=ab>Спойлер:</td><td class=spo>think [θiŋk] - thought [θɔ:t]", 
+"начинать</td></tr><tr><td class=ab>Спойлер:</td><td class=spo>start [sta:t] - started [sta:tid]", 
+"закончить</td></tr><tr><td class=ab>Спойлер:</td><td class=spo>finish [ˈfɪnɪʃ] - finished [ˈfɪnɪʃt]"
+];
+
+
+
